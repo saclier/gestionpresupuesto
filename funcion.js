931947a8e62eacd85638442aebb38c9b579a13bb
Lucas Saclier <saclier@gmail.com>
2022-10-05 10:08:55 -0300

    cambio nombre de variable

--- funcion.js
+++ funcion.js
@@ -1,11 +1,11 @@
 function calcularPromedio(){
 
-    var a,b;
+    var num1,num2;
     
-    a = parseInt(document.getElementById("num1").value);
-    b = parseInt(document.getElementById("num2").value);
+    num1 = parseInt(document.getElementById("num1").value);
+    num2 = parseInt(document.getElementById("num2").value);
     
-    promedio = (a + b)/2;
+    promedio = (num1 + num2)/2;
     
     document.getElementById("pprom").innerHTML= promedio;
     
