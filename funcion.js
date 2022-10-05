e9fe1c96ea0e9db52a7eb8bbdc332281db5e4185
Lucas Saclier <saclier@gmail.com>
2022-10-05 10:01:53 -0300

    cambio de prom a promedio

--- funcion.js
+++ funcion.js
@@ -5,8 +5,8 @@ function calcularPromedio(){
     a = parseInt(document.getElementById("num1").value);
     b = parseInt(document.getElementById("num2").value);
     
-    prom = (a + b)/2;
+    promedio = (a + b)/2;
     
-    document.getElementById("pprom").innerHTML= prom;
+    document.getElementById("pprom").innerHTML= promedio;
     
     }
